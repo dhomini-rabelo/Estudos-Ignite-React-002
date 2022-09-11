5f5cec5bb04eb92a272ddcc724e216782e69f7d8
dhomini-rabelo <dhominisalgado@gmail.com>
2022-09-11 08:21:26 -0300

    config project colors

--- projects/practice02/tailwind.config.cjs
+++ projects/practice02/tailwind.config.cjs
@@ -2,7 +2,33 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Yellow: {
+          100: '#f1e9c9',
+          500: '#dbac2c',
+          800: '#c47f17',
+        },
+        Purple: {
+          100: '#ebe5f9',
+          500: '#8047f8',
+          800: '#4b2995',
+        },
+        Gray: {
+          100: '#fafafa',
+          200: '#f3f2f2',
+          300: '#ededed',
+          500: '#e6e5e5',
+          800: '#d7d7d5',
+        },
+        Black: {
+          100: '#8d8686',
+          300: '#574f4d',
+          500: '#403937',
+          800: '#272221',
+        },
+      },
+    },
   },
   plugins: [],
 }
