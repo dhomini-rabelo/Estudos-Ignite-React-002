38e779110ebaece7aa04ac7fbd9ec123c62fe04a
dhomini-rabelo <dhominisalgado@gmail.com>
2022-09-11 17:00:22 -0300

    add header content

--- projects/practice02/src/pages/index/index.tsx
+++ projects/practice02/src/pages/index/index.tsx
@@ -1,5 +1,27 @@
+import { NavLink } from 'react-router-dom'
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+import { Header } from './styles'
+
 export function Index() {
   return (
-    <h1 className="text-3xl font-bold underline text-blue-500">Hello world!</h1>
+    <>
+      <Header.container className="flex justify-between items-center mt-8">
+        <img src="/logo.svg" alt="project-logo" />
+        <nav className="flex items-center justify-center">
+          <NavLink
+            to="/"
+            className="p-2 bg-Yellow-100 text-Yellow-800 rounded-md hover:opacity-80"
+          >
+            <Coffee size={22} weight="fill" />
+          </NavLink>
+          <NavLink
+            to="/carrinho"
+            className="p-2 bg-Yellow-100 text-Yellow-800 rounded-md hover:opacity-80 ml-2"
+          >
+            <ShoppingCart size={22} weight="fill" />
+          </NavLink>
+        </nav>
+      </Header.container>
+    </>
   )
 }
---
+++ projects/practice02/src/pages/index/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Header = {
+  container: styled.header`
+    a {
+      border: 2px solid transparent;
+    }
+    a.active {
+      border: 2px solid var(--Yellow-800);
+    }
+  `,
+}
