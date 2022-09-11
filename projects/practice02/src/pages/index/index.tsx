6b81c1ba8113ba9716d85ed296adce5e326c5f8e
dhomini-rabelo <dhominisalgado@gmail.com>
2022-09-11 17:00:50 -0300

    add ad content

--- projects/practice02/src/pages/index/index.tsx
+++ projects/practice02/src/pages/index/index.tsx
@@ -22,6 +22,58 @@ export function Index() {
           </NavLink>
         </nav>
       </Header.container>
+      <div className="mt-32 flex justify-between flex-wrap-reverse lg:flex-nowrap gap-y-8">
+        <div className="flex flex-col">
+          <h2 className="baloo text-5xl font-extrabold leading-tight text-Black-800">
+            Encontre o café perfeito para qualquer hora do dia
+          </h2>
+          <span className="text-xl leading-tight text-Black-500 mt-4">
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </span>
+          <div className="flex mt-16 flex-wrap gap-y-5 gap-x-10">
+            <div className="ad-box flex items-center justify-start">
+              <div className="p-2 bg-Yellow-800 text-Gray-100 rounded-full">
+                <ShoppingCart size={16} weight="fill" />
+              </div>
+              <span className="ml-3 text-Black-300">
+                Compra simples e segura
+              </span>
+            </div>
+            <div className="ad-box flex items-center justify-start">
+              <div className="p-2 bg-Black-300 text-Gray-100 rounded-full">
+                <Package size={16} weight="fill" />
+              </div>
+              <span className="ml-3 text-Black-300">
+                Embalagem mantém o café intacto
+              </span>
+            </div>
+          </div>
+          <div className="flex flex-wrap mt-5 gap-y-5 gap-x-10">
+            <div className="ad-box flex items-center justify-start">
+              <div className="p-2 bg-Yellow-500 text-Gray-100 rounded-full">
+                <Timer size={16} weight="fill" />
+              </div>
+              <span className="ml-3 text-Black-300">
+                Entrega rápida e rastreada
+              </span>
+            </div>
+            <div className="ad-box flex items-center justify-start">
+              <div className="p-2 bg-Purple-800 text-Gray-100 rounded-full">
+                <Coffee size={16} weight="fill" />
+              </div>
+              <span className="ml-3 text-Black-300">
+                O café chega fresquinho até você
+              </span>
+            </div>
+          </div>
+        </div>
+        <img
+          src="/coffee.svg"
+          alt="coffee-cup"
+          className="mx-auto inline-block"
+        />
+      </div>
     </>
   )
 }
