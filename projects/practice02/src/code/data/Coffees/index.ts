dab01d1dcb5e628210a59258bd4791f8ab44da2d
dhomini-rabelo <dhominisalgado@gmail.com>
2022-09-12 20:26:45 -0300

    add coffees

--- projects/practice02/src/code/data/Coffees/index.ts
+++ projects/practice02/src/code/data/Coffees/index.ts
@@ -9,4 +9,28 @@ export const Coffees: CoffeeType[] = [
     description: 'O tradicional café feito com água quente e grãos moídos',
     price: 9.9,
   },
+  {
+    id: 2,
+    fileUrl: 'coffees/expresso-americano.svg',
+    categories: ['TRADICIONAL'],
+    name: 'Expresso Americano',
+    description: 'Expresso diluído, menos intenso que o tradicional',
+    price: 4,
+  },
+  {
+    id: 3,
+    fileUrl: 'coffees/expresso-cremoso.svg',
+    categories: ['TRADICIONAL'],
+    name: 'Expresso Cremoso',
+    description: 'Café expresso tradicional com espuma cremosa',
+    price: 7.5,
+  },
+  {
+    id: 4,
+    fileUrl: 'coffees/expresso-gelado.svg',
+    categories: ['TRADICIONAL', 'GELADO'],
+    name: 'Expresso Gelado',
+    description: 'Bebida preparada com café expresso e cubos de gelo',
+    price: 7,
+  },
 ]
