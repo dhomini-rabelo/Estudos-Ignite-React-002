9086f55a49329c27f3ba1011cb082c68f054799d
dhomini-rabelo <dhominisalgado@gmail.com>
2022-09-15 22:23:38 -0300

    fs página de pedido

--- projects/practice02/src/pages/pedido/index.tsx
+++ projects/practice02/src/pages/pedido/index.tsx
@@ -1,3 +1,4 @@
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
 import { Div } from './styles'
 
 export function RequestFeedback() {
@@ -11,6 +12,35 @@ export function RequestFeedback() {
           <span className="leading-tight text-xl">
             Agora é só aguardar que logo o café chegará até você
           </span>
+        </div>
+        <div className="info-box p-10 flex flex-col gap-y-8">
+          <div className="flex items-center gap-x-3">
+            <div className="p-2 bg-Purple-800 text-white rounded-full">
+              <MapPin size={16} weight="fill" />
+            </div>
+            <span className="text-Black-300 leading-tight">
+              Entrega em <strong>Rua João Daniel Martinelli</strong>, 102
+              Farrapos - Porto Alegre, RS
+            </span>
+          </div>
+          <div className="flex items-center gap-x-3">
+            <div className="p-2 bg-Yellow-500 text-white rounded-full">
+              <Timer size={16} weight="fill" />
+            </div>
+            <div className="text-Black-300 flex flex-col items-start leading-tight">
+              <span>Previsão de entrega</span>
+              <strong>20 min - 30 min</strong>
+            </div>
+          </div>
+          <div className="flex items-center gap-x-3">
+            <div className="p-2 bg-Yellow-800 text-white rounded-full">
+              <CurrencyDollar size={16} weight="fill" />
+            </div>
+            <div className="text-Black-300 flex flex-col items-start leading-tight">
+              <span>Pagamento na entrega</span>
+              <strong>Cartão de Crédito</strong>
+            </div>
+          </div>
         </div>
       </Div.requestInfo>
       <img src="/sale.svg" alt="sale-image" />
--- projects/practice02/src/pages/pedido/styles.ts
+++ projects/practice02/src/pages/pedido/styles.ts
@@ -5,5 +5,17 @@ export const Div = {
     .title {
       font-size: 2rem;
     }
+
+    .info-box {
+      border-radius: 6px 36px;
+      width: 32.6875rem;
+      border: 1px solid;
+      border-image-source: linear-gradient(
+          102.89deg,
+          #dbac2c 2.61%,
+          #8047f8 98.76%
+        )
+        1px;
+    }
   `,
 }
