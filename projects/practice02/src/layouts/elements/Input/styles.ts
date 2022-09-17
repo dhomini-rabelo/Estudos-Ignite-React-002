73daa930dd8f5b0df6d98ec954db48a6952e48bb
dhomini-rabelo <dhominisalgado@gmail.com>
2022-09-16 21:13:06 -0300

    ad input effects

--- projects/practice02/src/layouts/elements/Input/styles.ts
+++ projects/practice02/src/layouts/elements/Input/styles.ts
@@ -2,6 +2,9 @@ import styled from 'styled-components'
 
 export const Input = {
   form: styled.input`
-    border: 1px solid #e6e5e5;
+    border: 1px solid var(--Gray-500);
+    &:focus {
+      border: 1px solid var(--Yellow-500);
+    }
   `,
 }
