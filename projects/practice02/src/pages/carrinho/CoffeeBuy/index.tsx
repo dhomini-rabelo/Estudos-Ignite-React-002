aa933272f5815027b1d53c68464b8f0799765aa7
dhomini-rabelo <dhominisalgado@gmail.com>
2022-09-18 14:14:09 -0300

    add coffeeBuy component

---
+++ projects/practice02/src/pages/carrinho/CoffeeBuy/index.tsx
@@ -0,0 +1,25 @@
+import { Trash } from 'phosphor-react'
+import { ProductType } from '../../../code/contexts/Cart/types'
+import { adaptMoneyValue } from '../../../code/utils/values'
+import { ProductCounter } from '../../../layouts/components/ProductCounter'
+
+export function CoffeeBuy({ coffee }: { coffee: ProductType }) {
+  return (
+    <div className="coffee-buy flex items-start">
+      <img src={coffee.fileUrl} alt="coffee-image" className="w-16 h-16" />
+      <div className="coffee-data flex flex-col justify-center grow pl-5">
+        <span className="leading-tight">{coffee.name}</span>
+        <div className="flex mt-2 gap-x-2">
+          <ProductCounter product={coffee} extraClasses="w-16 h-8" />
+          <button className="h-8 bg-Gray-500 hover:bg-Gray-800 px-2 rounded-md text-sm">
+            <Trash size={16} className="text-Purple-500 inline-block mr-1" />
+            <span>REMOVER</span>
+          </button>
+        </div>
+      </div>
+      <strong className="text-Black-800">
+        R$ {adaptMoneyValue((coffee.price * 100 * coffee.quantity) / 100)}
+      </strong>
+    </div>
+  )
+}
--- projects/practice02/src/pages/carrinho/index.tsx
+++ projects/practice02/src/pages/carrinho/index.tsx
@@ -3,19 +3,21 @@ import {
   CreditCard,
   CurrencyDollar,
   MapPinLine,
-  Minus,
   Money,
-  Plus,
-  Trash,
 } from 'phosphor-react'
 import { useContext } from 'react'
 import { SaleContext } from '../../code/contexts/Cart'
+import { ProductType } from '../../code/contexts/Cart/types'
 import * as Input from '../../layouts/elements/Input'
+import { CoffeeBuy } from './CoffeeBuy'
 import { Div } from './styles'
 
 export function Cart() {
   const { sale } = useContext(SaleContext)
-  console.log(sale)
+  const productsForBuy = sale.products.filter(
+    (product: ProductType) => product.quantity > 0,
+  )
+
   return (
     <>
       <main className="mt-16">
@@ -113,70 +115,12 @@ export function Cart() {
               Cafés selecionados
             </h2>
             <Div.coffeeBuyContainer className="bg-Gray-200 p-10 rounded-md">
-              <Div.coffeeBuy className="coffee-buy flex items-start">
-                <img
-                  src="/coffees/expresso-tradicional.svg"
-                  alt="coffee-image"
-                  className="w-16 h-16"
-                />
-                <div className="coffee-data flex flex-col justify-center grow pl-5">
-                  <span className="leading-tight">Expresso tradicional</span>
-                  <div className="flex mt-2 gap-x-2">
-                    <div className="p-2 rounded-md bg-Gray-500 flex items-center justify-center w-16 h-8">
-                      <button className="text-Purple-500 hover:text-Purple-800">
-                        <Minus size={14} weight="fill" />
-                      </button>
-                      <span className="text-Black-800 mx-1">
-                        <strong>0</strong>
-                      </span>
-                      <button className="text-Purple-500 hover:text-Purple-800">
-                        <Plus size={14} weight="fill" />
-                      </button>
-                    </div>
-                    <button className="h-8 bg-Gray-500 hover:bg-Gray-800 px-2 rounded-md text-sm">
-                      <Trash
-                        size={16}
-                        className="text-Purple-500 inline-block mr-1"
-                      />
-                      <span>REMOVER</span>
-                    </button>
-                  </div>
-                </div>
-                <strong className="text-Black-800">R$ 9,90</strong>
-              </Div.coffeeBuy>
-              <div className="border-separation my-6"></div>
-              <Div.coffeeBuy className="coffee-buy flex items-start">
-                <img
-                  src="/coffees/expresso-tradicional.svg"
-                  alt="coffee-image"
-                  className="w-16 h-16"
-                />
-                <div className="coffee-data flex flex-col justify-center grow pl-5">
-                  <span className="leading-tight">Expresso tradicional</span>
-                  <div className="flex mt-2 gap-x-2">
-                    <div className="p-2 rounded-md bg-Gray-500 flex items-center justify-center w-16 h-8">
-                      <button className="text-Purple-500 hover:text-Purple-800">
-                        <Minus size={14} weight="fill" />
-                      </button>
-                      <span className="text-Black-800 mx-1">
-                        <strong>0</strong>
-                      </span>
-                      <button className="text-Purple-500 hover:text-Purple-800">
-                        <Plus size={14} weight="fill" />
-                      </button>
-                    </div>
-                    <button className="h-8 bg-Gray-500 hover:bg-Gray-800 px-2 rounded-md text-sm">
-                      <Trash
-                        size={16}
-                        className="text-Purple-500 inline-block mr-1"
-                      />
-                      <span>REMOVER</span>
-                    </button>
-                  </div>
-                </div>
-                <strong className="text-Black-800">R$ 9,90</strong>
-              </Div.coffeeBuy>
-              <div className="border-separation my-6"></div>
+              {productsForBuy.map((product: ProductType) => (
+                <>
+                  <CoffeeBuy coffee={product} />
+                  <div className="border-separation my-6"></div>
+                </>
+              ))}
               <div className="mb-6 flex flex-col gap-y-3">
                 <div className="flex justify-between items-center text-Black-300 leading-tight">
                   <span className="text-sm">Itens</span>
